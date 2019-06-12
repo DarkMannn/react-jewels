af71bc06855e3b793ad33be43b08a3e639e90257
DarkMannn <mdarko1993@gmail.com>
2019-06-12 07:58:55 +0200

    Changed folder structure

--- src/App/components/board/board.js
+++ src/App/components/board/board.js
@@ -9,7 +9,6 @@ const BoardCss = css`
     grid-template: repeat(8, 1fr) / repeat(8, 1fr);
     grid-area: 3 / 4 / 8 / 9;
     gap: 0.2%;
-    ${'' /* border: 2px solid black; */}
 `;
 
 function Board() {
