dcf67b457299c36c9883971e9d4b7b6a8df7e5e3
DarkMannn <mdarko1993@gmail.com>
2019-06-17 23:29:11 +0200

    Changed jewel css to use styled.div.attrs()

--- src/App/components/jewel/jewel.js
+++ src/App/components/jewel/jewel.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { css } from 'styled-components';
+import styled from 'styled-components';
 import 'styled-components/macro';
 import { BOARD_HEIGHT } from '../../game-engine/engine.js';
 
-const JewelCss = css`
-    border: ${({ focused, combo }) => {
-        if (focused) return '4px solid lightgrey';
-        if (combo) return '4px solid black';
-        return '2px solid black'
-    }};
-    background-color: ${({ backgroundColor = 'darkgrey' }) => backgroundColor};
-    grid-area: ${({ x, y }) => `${BOARD_HEIGHT - y} / ${x + 1} / auto / auto`}
-`;
+const JewelDiv = styled.div.attrs(({ focused, combo, x, y, backgroundColor }) => ({
+    style: {
+        border: focused ? '4px solid lightgrey' : combo ? '4px solid black' : '2px solid black',
+        backgroundColor: backgroundColor || 'darkgrey',
+        gridArea: `${BOARD_HEIGHT - y} / ${x + 1} / auto / auto`
+    }
+}))``;
 
 function Jewel({
     backgroundColor, x, y, onItemClick, isFocused, isInCombo
 }) {
-    return <div
+    return <JewelDiv
         backgroundColor={backgroundColor}
         focused={isFocused}
         combo={isInCombo}
         x={x}
         y={y}
-        onClick={() => onItemClick(x, y)}
-        css={JewelCss}></div>;
+        onClick={() => onItemClick(x, y)}></JewelDiv>;
 };
 
 export default Jewel;
